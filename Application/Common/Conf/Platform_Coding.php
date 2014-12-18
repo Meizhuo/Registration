5120c4bbae022bafd5602e88cffc9f318c332e48
Jayin Ton <tonjayin@gmail.com>
2014-12-18 17:27:13 +0800

    change the db name

--- Application/Common/Conf/Platform_Coding.php
+++ Application/Common/Conf/Platform_Coding.php
@@ -3,7 +3,7 @@
 return array(//'配置项'=>'配置值'
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => '10.9.1.188', // 服务器地址
-    'DB_NAME' => 'enroll', // 数据库名
+    'DB_NAME' => 'cf_649cd643_5ce5_4641_8542_80485ce1fe52', // 数据库名
     'DB_USER' => 'vfzXu3G9LRYUqNe0', // 用户名
     'DB_PWD' => 'MhKDJLKSMy3BAOBt', // 密码
     'DB_PORT' => 3306, // 端口
