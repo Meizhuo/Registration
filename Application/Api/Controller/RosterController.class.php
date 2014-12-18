f44217e6dc2bff42e6fbf4f246ae5f31e8703ad4
Jayin Ton <tonjayin@gmail.com>
2014-12-18 15:23:50 +0800

    Roster 100%

--- Application/Api/Controller/RosterController.class.php
+++ Application/Api/Controller/RosterController.class.php
@@ -11,4 +11,13 @@ class RosterController extends ApiController{
         $this->reqPost(array('name','major','phone','student_id','grade','department','intro'));
         $this->ajaxReturn(D('Roster')->createRoster(I('post.')));
     }
+
+    public function pass(){
+        $this->reqPost(array('id'));
+        $this->ajaxReturn(D('Roster')->pass(I('post.id')));
+    }
+    public function unpass(){
+        $this->reqPost(array('id'));
+        $this->ajaxReturn(D('Roster')->unpass(I('post.id')));
+    }
 } 
