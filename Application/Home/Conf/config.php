07aa8341bd0f7e382a0234e2a53e898340b12753
Jayin Ton <tonjayin@gmail.com>
2014-12-24 17:03:48 +0800

    apply for wyu dev conf

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,6 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'URL_ROUTER_ON'   => true,// 开启路由
+    'URL_ROUTE_RULES' => require_once(MODULE_PATH . 'Conf/routers.php'),
 );
---
+++ Application/Home/Conf/routers.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'conf' => 'Index/conf'
+);
